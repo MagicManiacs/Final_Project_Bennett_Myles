f8be1d211735049c5649abb566f9765841adaeae
Myles Bennett <mylesbbennett@gmail.com>
2015-11-27 14:00:10 -0500

    Added Main Menu UI
    Game title and new game, high scores, and quit button all added.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    public string levelOne;
+    public string highScores;
+
+    public void NewGame()
+    {
+        Application.LoadLevel(levelOne);
+    }
+
+    public void HighScores()
+    {
+        Application.LoadLevel(highScores);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Game Exited");
+        Application.Quit();
+    }
+	
+}
